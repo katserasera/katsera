aed772b1f1b8ceae17163a8cd20fcaa187801f44
katserasera <katserara@gmail.com>
2026-07-19 20:23:28 +0700

    fix shadcn paths

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
-import { QueryProvider } from "@/providers/query-provider";
-import { Toaster } from "@/components/ui/sonner";
-
+import { QueryProvider } from "../providers/query-provider";
+import { Toaster } from "../components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Katsera",
